ffbbab34bb60f154d0d3e749cabfdc792b2ee179
cheessaar <cesgamer43@gmail.com>
2026-02-14 14:01:52 -0400

    Add files via upload

---
+++ constants/data.ts
@@ -0,0 +1,30 @@
+export const headerData = [
+  { title: "Inicio", href: "/" },
+  { title: "Tienda", href: "/shop" },
+  { title: "Blog", href: "/blog" },
+  { title: "Oferta Especial", href: "/deal" },
+  //   { title: "Contacto", href: "/contact" },
+];
+export const quickLinksData = [
+  { title: "Sobre nosotros", href: "/about" },
+  { title: "Contáctanos", href: "/contact" },
+  { title: "Términos y Condiciones", href: "/terms" },
+  { title: "Política de Privacidad", href: "/privacy" },
+  { title: "Preguntas Frecuentes", href: "/faqs" },
+  { title: "Ayuda", href: "/help" },
+];
+export const categoriesData = [
+  { title: "Móviles", href: "mobiles" },
+  { title: "Electrodomésticos", href: "appliances" },
+  { title: "Smartphones", href: "smartphones" },
+  { title: "Aire Acondicionado", href: "air-conditioners" },
+  { title: "Lavadoras", href: "washing-machine" },
+  { title: "Electrodomésticos de Cocina", href: "kitchen-appliances" },
+  { title: "Gadgets", href: "gadget-accessories" },
+];
+export const productType = [
+  { title: "Gadget", value: "gadget" },
+  { title: "Electrodomésticos", value: "appliances" },
+  { title: "Refrigeradores", value: "refrigerators" },
+  { title: "Otros", value: "others" },
+];
